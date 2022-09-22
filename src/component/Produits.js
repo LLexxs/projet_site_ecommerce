f8c4185702e22b3e767bab654f8a788d1b6ad722
Balixt <b00402600@gmail.com>
2022-09-22 17:53:20 +0200

    produits

--- src/component/Produits.js
+++ src/component/Produits.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 function Produits() {
-    return <div></div>
-
+  const Produits = [{img: '<a href="./img/amogus_amumu.webp">',
+                    nom: 'Amumu Among Us',
+                    commentaire: "N'avez-vous jamais rêvé de jouer à LoL ET à Among Us en même temps ?"
+                    }]
+    return (
+      <div>
+      {Produits.map((produit) => (
+        <li>
+          {produit.img}<br/>
+          {produit.nom}<br/>
+          {produit.commentaire}<br/>
+        </li>
+      ))}
+      </div>
+    )
 }
 
 export default Produits;
