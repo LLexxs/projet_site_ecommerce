b96713d7bbc56d3a20ebd4243c65ba73bd06569e
Alexandre D <a.domingues@ecole-ipssi.net>
2022-10-06 16:38:48 +0200

    modif merge

--- src/component/Produits.js
+++ src/component/Produits.js
@@ -1,46 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../style/Produits.css';
 import {Col} from "react-bootstrap";
 import {Row} from "react-bootstrap";
 import {Container} from "react-bootstrap";
-import {Link} from "react-router-dom";
-
 function Produits(props) {
 
-<<<<<<< HEAD
-
-function Produits() {
-    const Produits = [{
-        img: img,
-        nom: 'Saber Leona',
-        commentaire: "Fan de Fate ? (bof)"},
-        {img: img2,
-        nom: 'Amumu Among Us',
-        commentaire: "N'avez-vous jamais rêvé de jouer à LoL ET à Among Us en même temps ?"
-        },
-        {img: img3,
-        nom: 'Kindred Arclight',
-        commentaire: "Kindred n'aura jamais été aussi brillant qu'avec ce nouveau skin"
-        },
-        {img: img4,
-        nom: 'Yorick Minecraft',
-        commentaire: "Marre de creuser des tombes ? Creusez des blocs !"
-        }
-  ]
-    return (
-        <div className='main'>
-        <ul class="bas">
-
-              {Produits.map((produit) => (
-                <li class="header">
-                    <div><img src={produit.img} alt="lol"/></div>
-                    <div>{produit.nom}</div>
-                    <div>{produit.commentaire}</div>
-                </li>
-            ))}
-        </ul></div>
-=======
     return (
         <div className='main'>
             <Container>
@@ -59,7 +24,6 @@ function Produits() {
                 </Col>
             </Container>
         </div>
->>>>>>> 663ca0c2f8a459ea2099c101404dc202c4ce8e5a
     )
 }
 
